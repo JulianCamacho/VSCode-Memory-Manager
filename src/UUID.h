0c3ed09ca3f46f28dc46206a6daed2f3a26f5873
JulianCamacho <jjulian.341@gmail.com>
2020-06-09 19:18:41 -0600

    VMM-25 #comment Añadiendo generador de IDs (UUID) #resolve

---
+++ src/UUID.h
@@ -0,0 +1,17 @@
+//
+// Created by julian_camacho on 9/6/20.
+//
+
+#ifndef VSCODE_MEMORY_MANAGER_UUID_H
+#define VSCODE_MEMORY_MANAGER_UUID_H
+#include <string>
+#include <cstdlib>
+using namespace std;
+
+class UUID {
+public:
+    static string generateUUID();
+};
+
+
+#endif //VSCODE_MEMORY_MANAGER_UUID_H
